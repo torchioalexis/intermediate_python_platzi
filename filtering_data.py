ee41fab07aeaa53c7ac11a760754074c7292089b
Alexis Torchio <torchio.alexis@gmail.com>
2021-06-03 00:35:48 -0300

    Added example of filter and map

---
+++ filtering_data.py
@@ -0,0 +1,89 @@
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Héctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': 'Udemy',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run ():
+    python_devs = list(filter(lambda devs: devs["language"] == "python", DATA))
+    python_devs = list(map(lambda devs: devs["name"], python_devs))
+    not_platzi_devs = list(filter(lambda devs: devs["organization"] != "Platzi", DATA))
+    not_platzi_devs = list(map(lambda devs: devs["name"], not_platzi_devs))
+
+    print ("\nAplicando filtro a nuesto diccionario, podemos decir que los devs que saben programar en Python son:")
+    for developers in python_devs:
+        print (developers)
+
+    print ("\nPor otro lado, los devs que NO trabajan en Platzi son:")
+    for developers in not_platzi_devs:
+        print (developers)
+
+if __name__ == "__main__":
+    run ()
